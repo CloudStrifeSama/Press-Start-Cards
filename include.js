7ec3d82d058c7a7c8fae22540c910367f1ffe92b
CloudStrifeSama <guillaume.girard.1985@gmail.com>
2025-02-18 13:48:14 +0100

    Add files via upload

--- include.js
+++ include.js
@@ -1,15 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Chargement du header
     fetch('header.html')
         .then(response => response.text())
         .then(data => {
-            document.querySelector('header').innerHTML = data;
+            const headerElement = document.querySelector('header');
+            if (headerElement) {
+                headerElement.innerHTML = data;
+            } else {
+                console.error('Element <header> not found.');
+            }
         })
         .catch(error => console.error('Error loading header:', error));
 
+    // Chargement du footer
     fetch('footer.html')
         .then(response => response.text())
         .then(data => {
-            document.querySelector('footer').innerHTML = data;
+            const footerElement = document.querySelector('footer');
+            if (footerElement) {
+                footerElement.innerHTML = data;
+            } else {
+                console.error('Element <footer> not found.');
+            }
         })
         .catch(error => console.error('Error loading footer:', error));
 });
