c8dd269b58b4124c360bd0cdaab4c24dc768fb88
CloudStrifeSama <guillaume.girard.1985@gmail.com>
2025-02-19 11:30:51 +0100

    Delete header.php

--- header.php
+++
@@ -1,6 +0,0 @@
-<nav class="navbar">
-    <a href="index.html">Home</a>
-    <a href="cards.htm">Cards</a>
-    <a href="form.htm">Contact</a>
-    <a href="#" class="button-preorder">Pre-order now!</a>
-</nav>
