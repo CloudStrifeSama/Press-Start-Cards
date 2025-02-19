66c98596fb6bdc13ae9bdeb31eab531fe83a1adc
CloudStrifeSama <guillaume.girard.1985@gmail.com>
2025-02-19 15:59:53 +0100

    Create Includefr.js

---
+++ Includefr.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Chargement du header
+    fetch('headerfr.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok: ' + response.statusText);
+            }
+            return response.text();
+        })
+        .then(data => {
+            const headerElement = document.querySelector('head');
+            if (headerElement) {
+                headerElement.innerHTML = data;
+            } else {
+                console.error('Element <header> not found.');
+            }
+        })
+        .catch(error => console.error('Error loading header:', error));
+
+    // Chargement du footer
+    fetch('footerfr.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok: ' + response.statusText);
+            }
+            return response.text();
+        })
+        .then(data => {
+            const footerElement = document.querySelector('footer');
+            if (footerElement) {
+                footerElement.innerHTML = data;
+            } else {
+                console.error('Element <footer> not found.');
+            }
+        })
+        .catch(error => console.error('Error loading footer:', error));
+});
